d205ccda5db37cfe990212233402603121e17d16
nyver <nyver@mail.ru>
2013-11-08 18:00:20 +0400

    Initial commit

---
+++ src/com/nyver/idea/plugin/boiler/action/UploadClassAction.java
@@ -0,0 +1,18 @@
+package com.nyver.idea.plugin.boiler.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Upload to boiler action
+ *
+ * @author Yuri Novitsky
+ */
+public class UploadClassAction extends AnAction
+{
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent)
+    {
+
+    }
+}
